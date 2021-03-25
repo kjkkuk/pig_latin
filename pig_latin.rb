6db948b8df7fba8c50764cf14bd9d2e40b8898e7
Dmitry Yakutovich <kjk.kuk@mail.ru>
2021-03-25 21:32:39 +0300

    translation with two or more consonants

--- pig_latin.rb
+++ pig_latin.rb
@@ -4,6 +4,8 @@ require 'rubygems'
 require 'minitest/autorun'
 
 class PiglatinTranslatorTest < MiniTest::Test
+  VOWELS = %w[a e i o u].freeze
+
   def test_pig_becomes_igpay
     assert_equal 'igpay', translate('pig')
   end
@@ -20,32 +22,14 @@ class PiglatinTranslatorTest < MiniTest::Test
 #     assert_equal 'jej ejej ejej ejeje ejeje', translate('ekwjhfrkwjehfkjw')
 #   end
 
-    
-    VOWELS = %w[a e i o u].freeze
-
   def translate(word)
     first_letter = word[0]
-
-    # "igpay"
-    # return "eatay"
-
     if VOWELS.include?(first_letter)
-      ##
       word.split('')
-
-      # word.first = word.last
       "#{word}yay"
-
-    # 'eatyay'
     else
-      ##
-
-      # word[1..-1] + word[0]+ "ay"
-      # 'igpay'
-      index = word.split('').index { |x| x == VOWELS }
-
-      "#{word[index..-1]}#{word[0]}ay"
-
+      index = word.chars.index { |x| VOWELS.include? x }
+      "#{word[index..-1]}#{word[0..index - 1]}ay"
     end
   end
 end
